9627e0474722e8ca9fd76074ae448d422a59eff2
chandan <ck933970@gmail.com>
2023-07-13 23:12:42 -0700

    project setup and home and about components addeddd

---
+++ reactjs_forms/src/App.js
@@ -0,0 +1,20 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import About from "./About";
+function App() {
+  return (
+    <div>
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="about" element={<About />} />
+
+        </Routes>
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
